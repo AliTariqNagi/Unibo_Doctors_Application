b1711b6df41ea1bfdf27cf15fcc40dec02ffdf6c
AliTariqNagi <97218740+AliTariqNagi@users.noreply.github.com>
2025-04-28 13:43:28 +0200

    Update schemas.py for showing masks and images

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,20 @@
-from pydantic import BaseModel
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
 class DoctorImageValidationResponse(BaseModel):
-    id: Optional[int] = None  # Make the id field optional
+    id: Optional[int] = None
     image_path: str
+    mask_path: Optional[str] = None
     doctor_name: Optional[str] = None
     rating: Optional[int] = None
     comments: Optional[str] = None
+    mask_comments: Optional[str] = None
     disease_name: Optional[str] = None
     category: Optional[str] = None
+
+    class Config:
+        orm_mode = True
 
 class DoctorImageValidationRequest(BaseModel):
     doctor_name: str
@@ -22,10 +26,9 @@ class DoctorImageValidationRequest(BaseModel):
 
 class DoctorSchema(BaseModel):
     doctor_name: str
-    hospital: Optional[str]
-    hospital_address: Optional[str]
-    contact_number: Optional[str]
-    email: Optional[str]  # Use EmailStr for email validation
-    specialization: Optional[str]
+    hospital: Optional[str] = None
+    hospital_address: Optional[str] = None
+    contact_number: Optional[str] = None
+    email: Optional[str] = None  # Use EmailStr for email validation
+    specialization: Optional[str] = None
     registration_id: str
-

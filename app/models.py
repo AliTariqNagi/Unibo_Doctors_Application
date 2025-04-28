e41d72021f700e9e5bba8cdeda6373ffc0a680c8
AliTariqNagi <97218740+AliTariqNagi@users.noreply.github.com>
2025-04-28 13:43:07 +0200

    Update models.py for showing masks and images

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ import os
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import EmailStr
-
+from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, DateTime
 DATABASE_URL = "postgresql://admin:admin@localhost/doctor_image_validation"
 
 engine = create_engine(DATABASE_URL)
@@ -26,16 +27,35 @@ class Doctor(Base):
     registration_id = Column(String, nullable=False, unique=True)
 
 
+# class DoctorImageValidation(Base):
+#     __tablename__ = "doctor_image_validation"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     image_path = Column(String)
+#     doctor_name = Column(String, nullable=True)
+#     rating = Column(Integer, nullable=True)
+#     comments = Column(String, nullable=True)
+#     disease_name = Column(String)
+#     category = Column(String, nullable=True)  # Ensure this line exists
+
+
 class DoctorImageValidation(Base):
     __tablename__ = "doctor_image_validation"
 
     id = Column(Integer, primary_key=True, index=True)
     image_path = Column(String)
-    doctor_name = Column(String, nullable=True)
-    rating = Column(Integer, nullable=True)
-    comments = Column(String, nullable=True)
+    mask_path = Column(String)
+    doctor_name = Column(String)
+    rating = Column(Integer)
+    comments = Column(String)
+    mask_comments = Column(String)
     disease_name = Column(String)
-    category = Column(String, nullable=True)  # Ensure this line exists
+    category = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+
+
 
 # Create the database tables
 Base.metadata.create_all(bind=engine)
+
+

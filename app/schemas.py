c9a21bd49018ce7babdf102e2e692bb4e271ff6d
AliTariqNagi <ali.tariq.nagi@gmail.com>
2025-04-28 20:02:04 +0200

    changed indentation og schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,34 +1,34 @@
-    from typing import Optional
-    from pydantic import BaseModel, EmailStr
+from typing import Optional
+from pydantic import BaseModel, EmailStr
 
 
-    class DoctorImageValidationResponse(BaseModel):
-        id: Optional[int] = None
-        image_path: str
-        mask_path: Optional[str] = None
-        doctor_name: Optional[str] = None
-        rating: Optional[int] = None
-        comments: Optional[str] = None
-        mask_comments: Optional[str] = None
-        disease_name: Optional[str] = None
-        category: Optional[str] = None
+class DoctorImageValidationResponse(BaseModel):
+    id: Optional[int] = None
+    image_path: str
+    mask_path: Optional[str] = None
+    doctor_name: Optional[str] = None
+    rating: Optional[int] = None
+    comments: Optional[str] = None
+    mask_comments: Optional[str] = None
+    disease_name: Optional[str] = None
+    category: Optional[str] = None
 
-        class Config:
-            orm_mode = True
+    class Config:
+        orm_mode = True
 
-    class DoctorImageValidationRequest(BaseModel):
-        doctor_name: str
-        rating: int
-        comments: Optional[str] = None
-        disease_name: str
-        category: str
+class DoctorImageValidationRequest(BaseModel):
+    doctor_name: str
+    rating: int
+    comments: Optional[str] = None
+    disease_name: str
+    category: str
 
 
-    class DoctorSchema(BaseModel):
-        doctor_name: str
-        hospital: Optional[str] = None
-        hospital_address: Optional[str] = None
-        contact_number: Optional[str] = None
-        email: Optional[str] = None  # Use EmailStr for email validation
-        specialization: Optional[str] = None
-        registration_id: str
+class DoctorSchema(BaseModel):
+    doctor_name: str
+    hospital: Optional[str] = None
+    hospital_address: Optional[str] = None
+    contact_number: Optional[str] = None
+    email: Optional[str] = None  # Use EmailStr for email validation
+    specialization: Optional[str] = None
+    registration_id: str
